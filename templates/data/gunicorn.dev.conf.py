49128316660721b6b7222d6a18b8b5d518e9a133
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2017-01-24 23:31:00 +0900

    Cosmetic change

--- templates/data/gunicorn.dev.conf.py
+++ templates/data/gunicorn.dev.conf.py
@@ -203,9 +203,10 @@ def pre_exec(server):
 
 
 def on_starting(server):
-    # use server hook to patch socket to allow worker reloading
-    from gevent import monkey
-    monkey.patch_socket()
+    if worker_class == 'egg:gunicorn#gevent'
+        # use server hook to patch socket to allow worker reloading
+        from gevent import monkey
+        monkey.patch_socket()
 
 
 def when_ready(server):
@@ -219,10 +220,10 @@ def when_ready(server):
             modify_times = {}
             while True:
                 for module in sys.modules.values():
-                    path = getattr(module, "__file__", None)
+                    path = getattr(module, '__file__', None)
                     if not path:
                         continue
-                    if path.endswith(".pyc") or path.endswith(".pyo"):
+                    if path.endswith('.pyc') or path.endswith('.pyo'):
                         path = path[:-1]
                     try:
                         modified = os.stat(path).st_mtime
@@ -232,7 +233,7 @@ def when_ready(server):
                         modify_times[path] = modified
                         continue
                     if modify_times[path] != modified:
-                        logging.info("%s modified; restarting server", path)
+                        logging.info('%s modified; restarting server', path)
                         os.kill(os.getpid(), signal.SIGHUP)
                         modify_times = {}
                         break
